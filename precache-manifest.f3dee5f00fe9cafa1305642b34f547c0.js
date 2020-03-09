e5e99e2eb96699118071f33b417565bbfdb24f29
Kishore S <kishore@Kishores-MacBook-Pro.local>
2020-03-09 22:27:44 +0530

    Updates

---
+++ precache-manifest.f3dee5f00fe9cafa1305642b34f547c0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfac4cc3990131a0928c745dbaec7fe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f1a30be8c35e69dec5d",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ddf9b8d0fd1946245c36",
+    "url": "/static/css/main.3b23e247.chunk.css"
+  },
+  {
+    "revision": "2f1a30be8c35e69dec5d",
+    "url": "/static/js/2.24c45bdc.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.24c45bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf9b8d0fd1946245c36",
+    "url": "/static/js/main.f85f4d05.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  },
+  {
+    "revision": "3b8f27a47aafe47d1d0680d70041b5cc",
+    "url": "/static/media/indecision-app-screenshot.3b8f27a4.png"
+  },
+  {
+    "revision": "5d0a3c9be39aae2dacd1a1ff0b865155",
+    "url": "/static/media/library-app.5d0a3c9b.png"
+  },
+  {
+    "revision": "979853c9641cbc298554e35b58361b24",
+    "url": "/static/media/picture.979853c9.jpg"
+  },
+  {
+    "revision": "6597aba62aabd8cc763e304edc46554c",
+    "url": "/static/media/short-news.6597aba6.png"
+  },
+  {
+    "revision": "498aab2b3beb2c08d07aedd392b7375b",
+    "url": "/static/media/tea-cozy-screenshot.498aab2b.png"
+  }
+]);
